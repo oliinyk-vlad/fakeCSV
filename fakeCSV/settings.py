6226c83159d06f673c461efe209b1c6a9e77e54a
Vladyslav Oliinyk <oliinyk.vladyslav@gmail.com>
2020-08-15 00:21:05 +0300

    cors support added

--- fakeCSV/settings.py
+++ fakeCSV/settings.py
@@ -19,6 +19,10 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ORIGIN_WHITELIST = (
+    'https://fake-csv.s3.amazonaws.com'
+)
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -30,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'storages',
+    'corsheaders',
 
     'crispy_forms',
 
@@ -39,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
